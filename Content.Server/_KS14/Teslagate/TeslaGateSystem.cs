fcc68df93ce70048f0b25f8e2e19776d5ea7316a
LaCumbiaDelCoronavirus <90893484+LaCumbiaDelCoronavirus@users.noreply.github.com>
2025-11-23 15:34:50 +0800

    pro?

--- Content.Server/_KS14/Teslagate/TeslaGateSystem.cs
+++ Content.Server/_KS14/Teslagate/TeslaGateSystem.cs
@@ -4,123 +4,20 @@
 // SPDX-License-Identifier: MPL-2.0
 
 using Content.Shared._KS14.TeslaGate;
-using Content.Shared.Damage;
-using Robust.Shared.Physics.Events;
-using Robust.Shared.Physics.Systems;
-using Robust.Shared.Timing;
-using System.Runtime.CompilerServices;
 using Content.Server.AlertLevel;
-using DependencyAttribute = Robust.Shared.IoC.DependencyAttribute;
+using Content.Server.Station.Systems;
 
 namespace Content.Server._KS14.TeslaGate;
 
 public sealed class TeslaGateSystem : SharedTeslaGateSystem
 {
-    [Dependency] private readonly IGameTiming _gameTiming = default!;
-    [Dependency] private readonly SharedPhysicsSystem _physicsSystem = default!;
-    [Dependency] private readonly DamageableSystem _damageableSystem = default!;
+    [Dependency] private readonly StationSystem _stationSystem = default!;
 
     public override void Initialize()
     {
         base.Initialize();
 
-        SubscribeLocalEvent<TeslaGateComponent, StartCollideEvent>(OnGateStartCollide);
         SubscribeLocalEvent<AlertLevelChangedEvent>(OnAlertLevelChanged);
-    }
-
-    public override void Update(float frameTime)
-    {
-        base.Update(frameTime);
-
-        var query = EntityQueryEnumerator<TeslaGateComponent>();
-        while (query.MoveNext(out var uid, out var teslaGateComponent))
-        {
-            var teslaGate = (uid, teslaGateComponent);
-            var canShock = CanWork(uid, teslaGateComponent, out var canStart);
-
-            if (teslaGateComponent.IsTimerWireCut)
-                continue;
-
-            if (teslaGateComponent.CurrentlyShocking)
-            {
-                if (!canShock || IsFinishedShocking(teslaGateComponent))
-                    QuitZappinEmAll(teslaGate);
-
-                continue;
-            }
-
-            if (!canStart)
-                continue;
-
-            if (_gameTiming.CurTime < teslaGateComponent.NextPulse)
-                continue;
-
-            if (canShock)
-                ZapEmAll(teslaGate);
-        }
-    }
-
-    // HELP
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private bool CanWork(EntityUid uid, TeslaGateComponent teslaGateComponent, out bool canStart)
-    {
-        canStart = CanStartWork(uid);
-        if (!teslaGateComponent.Enabled)
-            return false;
-
-        if (!canStart)
-            return false;
-
-        return true;
-    }
-
-    private void ZapEmAll(Entity<TeslaGateComponent> teslaGate)
-    {
-        var (uid, teslaGateComponent) = teslaGate;
-        teslaGateComponent.LastShockTime = _gameTiming.CurTime;
-        teslaGateComponent.NextPulse = _gameTiming.CurTime + teslaGate.Comp.PulseInterval;
-
-        AudioSystem.PlayPvs(teslaGateComponent.ShockSound, uid);
-
-        UpdateAppearance(teslaGate, true);
-        Dirty(teslaGate);
-
-        teslaGateComponent.CurrentlyShocking = true;
-        foreach (var entity in _physicsSystem.GetContactingEntities(uid))
-            CollideAct(teslaGateComponent, entity);
-    }
-
-    private void QuitZappinEmAll(Entity<TeslaGateComponent> teslaGate)
-    {
-        var (uid, teslaGateComponent) = teslaGate;
-
-        teslaGateComponent.CurrentlyShocking = false;
-        teslaGateComponent.ThingsBeingShocked.Clear();
-
-        UpdateAppearance(teslaGate, false);
-        Dirty(teslaGate);
-
-        AudioSystem.PlayPvs(teslaGateComponent.StartingSound, uid);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private void Zap(EntityUid uid, DamageSpecifier damage)
-    {
-        _damageableSystem.TryChangeDamage(uid, damage, ignoreResistances: true);
-    }
-
-    private void CollideAct(TeslaGateComponent teslaGateComponent, EntityUid otherEntity)
-    {
-        if (!teslaGateComponent.ThingsBeingShocked.Add(GetNetEntity(otherEntity)))
-            return;
-
-        Zap(otherEntity, teslaGateComponent.ShockDamage);
-    }
-
-    private void OnGateStartCollide(Entity<TeslaGateComponent> teslaGate, ref StartCollideEvent args)
-    {
-        if (teslaGate.Comp.CurrentlyShocking)
-            CollideAct(teslaGate, args.OtherEntity);
     }
 
     private void OnAlertLevelChanged(AlertLevelChangedEvent alertEvent)
@@ -130,14 +27,19 @@ public sealed class TeslaGateSystem : SharedTeslaGateSystem
 
         var alertLevel = alertEvent.AlertLevel;
 
-        var query = EntityQueryEnumerator<TeslaGateComponent>();
-        while (query.MoveNext(out var uid, out var teslaGateComponent))
+        var query = EntityQueryEnumerator<TeslaGateComponent, TransformComponent>();
+        while (query.MoveNext(out var uid, out var teslaGateComponent, out var transformComponent))
         {
-            var teslaGate = (uid, teslaGateComponent);
+            // only process this teslagate if: it's on a station, and the station that it's on is the one that this AlertLevelChangedEvent is directed at
+            if (transformComponent.GridUid == null ||
+                _stationSystem.GetOwningStation(uid, transformComponent) is not { } owningStationUid ||
+                owningStationUid != alertEvent.Station)
+                continue;
 
             if (teslaGateComponent.IsForceHacked)
                 continue;
 
+            var teslaGate = (uid, teslaGateComponent);
             if (teslaGateComponent.EnabledAlertLevels.Contains(alertLevel))
                 Enable(teslaGate);
             else
--- Content.Shared/_KS14/Teslagate/SharedTeslaGateSystem.cs
+++ Content.Shared/_KS14/Teslagate/SharedTeslaGateSystem.cs
@@ -4,9 +4,12 @@
 // SPDX-License-Identifier: MPL-2.0
 
 using System.Runtime.CompilerServices;
+using Content.Shared.Damage;
 using Content.Shared.Power;
 using Content.Shared.Power.EntitySystems;
 using Robust.Shared.Audio.Systems;
+using Robust.Shared.Physics.Events;
+using Robust.Shared.Physics.Systems;
 using Robust.Shared.Timing;
 using DependencyAttribute = Robust.Shared.IoC.DependencyAttribute;
 
@@ -17,6 +20,8 @@ public abstract class SharedTeslaGateSystem : EntitySystem
 {
     [Dependency] protected readonly SharedAudioSystem AudioSystem = default!;
     [Dependency] private readonly IGameTiming _gameTiming = default!;
+    [Dependency] private readonly SharedPhysicsSystem _physicsSystem = default!;
+    [Dependency] private readonly DamageableSystem _damageableSystem = default!;
     [Dependency] private readonly SharedAppearanceSystem _appearanceSystem = default!;
     [Dependency] private readonly SharedPointLightSystem _pointLight = default!;
     [Dependency] private readonly SharedPowerReceiverSystem _powerReceiverSystem = default!;
@@ -24,7 +29,105 @@ public abstract class SharedTeslaGateSystem : EntitySystem
     public override void Initialize()
     {
         base.Initialize();
+
+        SubscribeLocalEvent<TeslaGateComponent, StartCollideEvent>(OnGateStartCollide);
         SubscribeLocalEvent<TeslaGateComponent, PowerChangedEvent>(OnPowerChange);
+    }
+
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var query = EntityQueryEnumerator<TeslaGateComponent>();
+        while (query.MoveNext(out var uid, out var teslaGateComponent))
+        {
+            var teslaGate = (uid, teslaGateComponent);
+            var canShock = CanWork(uid, teslaGateComponent, out var canStart);
+
+            if (teslaGateComponent.IsTimerWireCut)
+                continue;
+
+            if (teslaGateComponent.CurrentlyShocking)
+            {
+                if (!canShock || IsFinishedShocking(teslaGateComponent))
+                    QuitZappinEmAll(teslaGate);
+
+                continue;
+            }
+
+            if (!canStart)
+                continue;
+
+            if (_gameTiming.CurTime < teslaGateComponent.NextPulse)
+                continue;
+
+            if (canShock)
+                ZapEmAll(teslaGate);
+        }
+    }
+
+    // HELP
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private bool CanWork(EntityUid uid, TeslaGateComponent teslaGateComponent, out bool canStart)
+    {
+        canStart = CanStartWork(uid);
+        if (!teslaGateComponent.Enabled)
+            return false;
+
+        if (!canStart)
+            return false;
+
+        return true;
+    }
+
+    private void ZapEmAll(Entity<TeslaGateComponent> teslaGate)
+    {
+        var (uid, teslaGateComponent) = teslaGate;
+        teslaGateComponent.LastShockTime = _gameTiming.CurTime;
+        teslaGateComponent.NextPulse = _gameTiming.CurTime + teslaGate.Comp.PulseInterval;
+
+        AudioSystem.PlayLocal(teslaGateComponent.ShockSound, uid, soundInitiator: null);
+
+        UpdateAppearance(teslaGate, true);
+        Dirty(teslaGate);
+
+        teslaGateComponent.CurrentlyShocking = true;
+        foreach (var entity in _physicsSystem.GetContactingEntities(uid))
+            CollideAct(teslaGateComponent, entity);
+    }
+
+    private void QuitZappinEmAll(Entity<TeslaGateComponent> teslaGate)
+    {
+        var (uid, teslaGateComponent) = teslaGate;
+
+        teslaGateComponent.CurrentlyShocking = false;
+        teslaGateComponent.ThingsBeingShocked.Clear();
+
+        UpdateAppearance(teslaGate, false);
+        Dirty(teslaGate);
+
+        AudioSystem.PlayLocal(teslaGateComponent.StartingSound, uid, soundInitiator: null);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void Zap(EntityUid uid, DamageSpecifier damage)
+    {
+        _damageableSystem.TryChangeDamage(uid, damage, ignoreResistances: true);
+    }
+
+    private void CollideAct(TeslaGateComponent teslaGateComponent, EntityUid otherEntity)
+    {
+        if (!teslaGateComponent.ThingsBeingShocked.Add(GetNetEntity(otherEntity)))
+            return;
+
+        Zap(otherEntity, teslaGateComponent.ShockDamage);
+    }
+
+    private void OnGateStartCollide(Entity<TeslaGateComponent> teslaGate, ref StartCollideEvent args)
+    {
+        if (teslaGate.Comp.CurrentlyShocking)
+            CollideAct(teslaGate, args.OtherEntity);
     }
 
     public bool IsFinishedShocking(TeslaGateComponent teslaGateComponent) => _gameTiming.CurTime > teslaGateComponent.LastShockTime + teslaGateComponent.ShockLength;
